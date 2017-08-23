8fa8ec541c0ec4a3cd2dbeab2ed4cd09451b1c0f
DheemanthRegati <31275748+DheemanthRegati@users.noreply.github.com>
2017-08-23 16:22:08 +0530

    Add files via upload

---
+++ 1stq.cpp
@@ -0,0 +1,11 @@
+    #include <iostream>
+    using namespace std;
+     
+    int main() {int num=10; char alb='d'; float dec= 3.14; bool alg= true;
+    cout << num<<endl;
+    cout << alb<<endl;
+    cout << dec<<endl;
+    cout << alg<<endl;
+     
+    	return 0;
+    }
