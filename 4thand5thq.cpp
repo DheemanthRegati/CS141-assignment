622d9711c74933d1c8a112e2c2b7cf11a0531382
DheemanthRegati <31275748+DheemanthRegati@users.noreply.github.com>
2017-08-24 19:29:46 +0530

    Add files via upload

---
+++ 4thand5thq.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int l,b,p,a;
+    
+    cout<<"enter length";
+    cin>>l;
+    cout<<"enter breadth";
+    cin>>b;
+    p=2*(l+b);//i hope its fine to write 1 program for 2 questions
+    a=l*b;
+    cout<<"Perimeter is "<<p<<" and the area is "<<a;
+    return 0;
+}
+    
+    
